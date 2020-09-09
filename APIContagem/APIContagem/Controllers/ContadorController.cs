bcfb8bf1bcaad049815e3f1cf1f49062d78a0d73
Daniel Abreu <daniel.snipher@gmail.com>
2020-09-09 15:22:25 -0300

    1.0

---
+++ APIContagem/APIContagem/Controllers/ContadorController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace APIContagem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContadorController : ControllerBase
+    {
+        private static readonly Contador _CONTADOR = new Contador();
+
+        private readonly ILogger<ContadorController> _logger;
+
+        public ContadorController(ILogger<ContadorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public OkObjectResult Get()
+        {
+            lock (_CONTADOR)
+            {
+                _CONTADOR.Incrementar();
+                _logger.LogInformation($"Contador - Valor atual: {_CONTADOR.ValorAtual}");
+
+                return new OkObjectResult(new
+                {
+                    _CONTADOR.ValorAtual,
+                    _CONTADOR.Local,
+                    _CONTADOR.Kernel,
+                    _CONTADOR.TargetFramework,
+                    MensagemFixa = "Teste local",
+                    MensagemVariavel = Environment.GetEnvironmentVariable("MensagemVariavel")
+                });
+            }
+        }
+    }
+}
