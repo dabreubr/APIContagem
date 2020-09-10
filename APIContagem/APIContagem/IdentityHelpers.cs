03557ee204f8af1645d8b9e0b53a598fb92b269e
Daniel Abreu <daniel.snipher@gmail.com>
2020-09-09 23:14:53 -0300

    permissions

---
+++ APIContagem/APIContagem/IdentityHelpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading.Tasks;
+
+namespace APIContagem
+{
+    public static class IdentityHelpers
+    {
+        public static string ShortName(this WindowsIdentity Identity)
+        {
+            if (null != Identity)
+            {
+                return Identity.Name.Split(new char[] { '\\' })[1];
+            }
+            return string.Empty;
+        }
+    }
+}
